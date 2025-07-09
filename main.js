72dd824c1a8455dd312a0d2fe359f1e291f7779a
Ntokozo <ntokozostevenmbuli035@gmail.com>
2025-07-09 08:27:28 -0900

    Canvass

---
+++ main.js
@@ -0,0 +1,19 @@
+const canvas = document.getElementById("canvas");
+
+function draw() {
+  const ctx = canvas.getContext("2d");
+  ctx.fillRect(25, 25, 100, 100);
+  ctx.fill();
+  console.log(ctx);
+}
+
+draw();
+
+function play() {
+  const video = document.getElementById("webcam");
+  navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+    video.srcObject = stream;
+  });
+}
+
+play();
